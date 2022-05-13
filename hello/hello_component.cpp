186327b4fbc7d2458ce241665055ea581060dbfd
samarrhilane <samarrhilane@gmail.com>
2022-05-13 19:59:14 +0200

    Update hello_component.cpp

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -32,8 +32,8 @@ PYBIND11_MODULE(hello_component,greetings)
   //greetings.def("Hello_IF", &Hello_IF, "a function saying hi Miage IF");
   greetings.def("getVersion", &getVersion, "a function returning the version");
 	
-  py::class_<Himiage>(greetings, "Himiage")
-	.def(py::init<std::string>())
+  py::class_<Himiage>(greetings, "Himiage",py::dynamic_attr())
+	.def(py::init<>())
 	.def("Hello_IF", &Himiage::Hello_IF);
 }
 
