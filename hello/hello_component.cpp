9e4bd6eb794e10a01c95e4ef8d44dd27eab831e2
samarrhilane <samarrhilane@gmail.com>
2022-05-13 18:33:49 +0200

    Définition d’un objet python en C++

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -10,34 +10,32 @@ char const* greet() {
   return "hello, world";
 }
 
-char const* Hello_IF() {
-  return "Hi, Miage IF";
+class Himiage 
+{
+     public:
+	 ~Himiage() { }
+	 char const* Hello_IF() {
+ 	 return "Hi, Miage IF";
 }
 
 namespace py = pybind11;
 
+	
+	
 
 PYBIND11_MODULE(hello_component,greetings)
 {
   greetings.doc() = "greeting_object 1.0";
   
   greetings.def("greet", &greet, "a function saying hello");
-  greetings.def("Hello_IF", &Hello_IF, "a function saying hi Miage IF");
+  //greetings.def("Hello_IF", &Hello_IF, "a function saying hi Miage IF");
   greetings.def("getVersion", &getVersion, "a function returning the version");
+	
+  py::class_<Himiage>(greetings, "Himiage")
+	.def(py::init<>())
+	.def("Hello_IF", &Himiage::Hello_IF);
 }
 
-class Animal {
-public:
-    virtual ~Animal() { }
-    virtual std::string go(int n_times) = 0;
-};
 
-class Dog : public Animal {
-public:
-    std::string go(int n_times) override {
-        std::string result;
-        for (int i=0; i<n_times; ++i)
-            result += "woof! ";
-        return result;
-    }
-};
+
+
